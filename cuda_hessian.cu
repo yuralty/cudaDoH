4340292e92e79ef1ecf43699bf8f23eb122c6412
yuralty <yuralty@gmail.com>
2015-01-15 19:32:26 +0800

    first commit
    non-paralleled version complete

---
+++ cuda_hessian.cu
@@ -0,0 +1,156 @@
+/*
+This is the CUDA implementation of hessian.cpp
+
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+
+
+#include "responselayer.h"
+
+
+using namespace std;
+
+const int width = 640;
+const int height = 466;
+
+const int OCTAVES = 4;
+const int INTERVALS = 4;
+const int step = 1;
+int scales[OCTAVES][INTERVALS] = {{9,15,21,27}, {15,27,39,51}, {27,51,75,99},
+51,99,147,195};
+
+void loadImg(float *, char *, int, int);
+void buildResponseLayer(float *, ResponseLayer *);
+float BoxIntegral(float *, int, int, int, int);
+
+
+void loadImg(float *img, char* fname, int w, int h)
+{
+    
+    ifstream readstream;
+    readstream.open(fname);
+    char comma;
+    for(int i=0; i<w*h; ++i) {
+        readstream >> img[i];
+        readstream >> comma;
+    }
+    readstream.close();
+}
+
+/*void checkImg(float *img, int r, int c) */
+/*{*/
+    /*float res = img[r*width + c];*/
+    /*printf("%f\n", res);*/
+/*}*/
+
+
+
+void buildResponseLayer(float* img, ResponseLayer *rl)
+{
+    float *responses = rl->responses;
+    int step = rl->step;
+    int b = (rl->filter - 1) / 2 + 1;
+    int l = rl->filter / 3;
+    int w = rl->filter;
+    float inverse_area = 1.f/(w*w);
+    float Dxx, Dyy, Dxy;
+
+    for(int r, c, ar = 0, index = 0; ar < rl->height; ++ar)
+    {
+        for(int ac = 0; ac < rl->width; ++ac, ++index)
+        {
+            r = ar * step;
+            c = ac * step;
+
+            Dxx = BoxIntegral(img, r - l + 1, c - b, 2*l - 1, w)
+                    - BoxIntegral(img, r - l + 1, c - l / 2, 2*l - 1, l)*3;
+            Dyy = BoxIntegral(img, r - b, c - l + 1, w, 2*l - 1)
+                    - BoxIntegral(img, r - l / 2, c - l + 1, l, 2*l - 1)*3;
+            Dxy = + BoxIntegral(img, r - l, c + 1, l, l)
+                    + BoxIntegral(img, r + 1, c - l, l, l)
+                    - BoxIntegral(img, r - l, c - l, l, l)
+                    - BoxIntegral(img, r + 1, c + 1, l, l);
+
+            Dxx *= inverse_area;
+            Dyy *= inverse_area;
+            Dxy *= inverse_area;
+
+            responses[index] = (Dxx * Dyy - 0.81f *Dxy * Dxy);
+        }
+    }
+}
+
+void checkResponse(ResponseLayer *rl)
+{
+    printf("response for layer w:%d h:%d s:%d oct:%d\n", rl->width, rl->height,
+            rl->step, rl->filter);
+    float * responses = rl->responses;
+    
+    for(int i=0; i<rl->height; ++i)
+    {
+        for(int j=0; j<rl->width; ++j)
+            printf("%f ",responses[i*rl->width+j]);
+        printf("\n");
+    }
+
+}
+
+float BoxIntegral(float* img, int row, int col, int rows, int cols)
+{
+    int r1 = min(row, height) - 1;
+    int c1 = min(col, width) - 1;
+    int r2 = min(row + rows, height) - 1;
+    int c2 = min(col + cols, width) - 1;
+
+    float A(0.0f), B(0.0f), C(0.0f), D(0.0f);
+    if (r1 >=0 && c1 >= 0) A = img[r1*step + c1];
+    if (r1 >=0 && c2 >= 0) B = img[r1*step + c2];
+    if (r2 >=0 && c1 >= 0) C = img[r2*step + c1];
+    if (r2 >=0 && c2 >= 0) D = img[r2*step + c2];
+
+    return max(0.f, A - B - C + D);
+}
+
+
+
+int main()
+{
+    // load integral image
+    float *img;
+    img = new float[width*height];
+    char* testfile = "integral.csv";
+    loadImg(img, testfile, width, height);
+
+    // build response map
+    vector<ResponseLayer *> responseMap;
+    
+    for (int oct=0; oct<OCTAVES; ++oct) {
+        for (int inter=0; inter<INTERVALS; ++inter) {
+            responseMap.push_back(new ResponseLayer (width/(int) pow(2.0,oct),
+                        height/(int) pow(2.0,oct), step*(int)pow(2.0,oct), scales[oct][inter]));
+        }
+    }
+
+    // nonparallel implementation
+    for(int i=0; i<responseMap.size(); ++i) {
+        ResponseLayer *tmp = responseMap[i];
+        /*printf("%d %d %d %d\n", tmp->width, tmp->height, tmp->step,*/
+            /*tmp->filter);*/
+        buildResponseLayer(img, responseMap[i]);
+    }
+
+    
+    checkResponse(responseMap[0]);
+
+
+
+    return 0;
+}
+
+
---
+++ responselayer.h
@@ -0,0 +1,38 @@
+
+class ResponseLayer
+{
+public:
+
+  int width, height, step, filter;
+  float *responses;
+
+  ResponseLayer(int width, int height, int step, int filter)
+  {
+    
+    this->width = width;
+    this->height = height;
+    this->step = step;
+    this->filter = filter;
+
+    responses = new float[width*height];
+
+    memset(responses,0,sizeof(float)*width*height);
+  }
+
+  ~ResponseLayer()
+  {
+    if (responses) delete [] responses;
+  }
+
+
+  inline float getResponse(unsigned int row, unsigned int column)
+  {
+    return responses[row * width + column];
+  }
+
+  inline float getResponse(unsigned int row, unsigned int column, ResponseLayer *src)
+  {
+    int scale = this->width / src->width;
+    return responses[(scale * row) * width + (scale * column)];
+  }
+};
